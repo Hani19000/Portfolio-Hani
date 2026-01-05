1931d5a3fe5548d1dab114d4931f447d5385b4f9
Hani <hanider27@gmail.com>
2026-01-05 01:25:45 +0100

    mise a jours

--- frontend/src/components/portfolio/PortfolioCards.ts
+++ frontend/src/components/portfolio/PortfolioCards.ts
@@ -1,10 +1,10 @@
-import IMG1 from "../../assets/portfolio1.png";
-import IMG2 from "../../assets/portfolio2.png";
-import IMG3 from "../../assets/portfolio3.png";
-import IMG4 from "../../assets/portfolio4.png";
-import IMG5 from "../../assets/portfolio5.png";
-import IMG6 from "../../assets/portfolio6.png";
-import IMG7 from "../../assets/portfolio7.png";
+import IMG1 from "../../assets/portfolio1.webp";
+import IMG2 from "../../assets/portfolio2.webp";
+import IMG3 from "../../assets/portfolio3.webp";
+import IMG4 from "../../assets/portfolio4.webp";
+import IMG5 from "../../assets/portfolio5.webp";
+import IMG6 from "../../assets/portfolio6.webp";
+import IMG7 from "../../assets/portfolio7.webp";
 
 /* Définition de l'interface */
 
