17e2b2dc40a87ff57e8ccf3e42682e1f4024adee
Hani <hanider27@gmail.com>
2026-01-07 22:15:23 +0100

    mise a jours

--- frontend/src/components/header/components/CTA.tsx
+++ frontend/src/components/header/components/CTA.tsx
@@ -3,7 +3,7 @@ import CV from "../../../assets/cv.pdf";
 
 const CTA: React.FC = () => (
   <div className="cta">
-    <a href={CV} target='_blank' rel="noreferrer" className='btn'>Télécharger Mon CV</a>
+    <a href={CV} target='_blank' rel="noreferrer" className='btn'>Visualiser Mon CV</a>
     <a href="#contact" className='btn btn-primary'>Me Contacter</a>
   </div>
 );
