82a1029d1e6c5f92c3e159ca6086032b9b488933
Hani <hanider27@gmail.com>
2026-01-09 23:18:25 +0100

    mise a jours

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Génère les fichiers de correspondance pour Sentry
+    sourcemap: true, 
+  }
 })
