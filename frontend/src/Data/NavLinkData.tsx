190aedfa84a56dc38caf1cf64e4c7d299f0539a9
Hani <hanider27@gmail.com>
2026-01-11 00:41:04 +0100

    mise a jours

--- frontend/src/Data/NavLinkData.tsx
+++ frontend/src/Data/NavLinkData.tsx
@@ -1,23 +1,22 @@
 import { ReactNode } from "react";
 import { IoMdHome, IoMdContact } from "react-icons/io";
 import { FaUser, FaBriefcase } from "react-icons/fa";
-import { FaBookOpen, FaStarHalfStroke } from "react-icons/fa6";
+import { FaBookOpen } from "react-icons/fa6";
 import { RiServiceFill } from "react-icons/ri";
 
-/* Interface pour un lien de navigation */
 export interface NavItem {
   id: string;
   icon: ReactNode;
 }
 
 const NavData: NavItem[] = [
-  { id: "#", icon: <IoMdHome /> },
+  // Ajout de la classe "icon-large" sur les icônes ciblées
+  { id: "#", icon: <IoMdHome className="nav__icon-large" /> },
   { id: "#about", icon: <FaUser /> },
   { id: "#experience", icon: <FaBookOpen /> },
   { id: "#services-section", icon: <RiServiceFill /> },
   { id: "#portfolio", icon: <FaBriefcase /> },
-  // { id: '#testimonials', icon: <FaStarHalfStroke /> },
-  { id: "#contact", icon: <IoMdContact /> },
+  { id: "#contact", icon: <IoMdContact className="nav__icon-large" /> },
 ];
 
 export default NavData;
