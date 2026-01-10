641f5d47db0ebc965cc312e687214572169c8ee2
Hani <hanider27@gmail.com>
2026-01-11 00:44:04 +0100

    mise a jours

--- frontend/src/pages/Header.tsx
+++ frontend/src/pages/Header.tsx
@@ -9,7 +9,7 @@ const Header: React.FC = () => (
     <HeaderParticles count={20} />
     <HeaderContent
       name="Hani Derrouiche"
-      title="Des Idées Au Dode, Donnons Vie À Vos Projets"
+      title="Des Idées Au Code, Donnons Vie À Vos Projets"
       role="Développeur Web Full-Stack"
       description="Un projet en tête ? Discutons-en pour voir comment je peux vous aider à le concrétiser."
       image={ME}
