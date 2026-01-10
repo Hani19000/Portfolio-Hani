c4f7a7958ae517a6b4402d018f85e5ec2bfea4d6
Hani <hanider27@gmail.com>
2026-01-10 15:57:56 +0100

    mise a jours

--- backend/middleware/ratelimiter.ts
+++ backend/middleware/ratelimiter.ts
@@ -1,7 +1,7 @@
 import rateLimit from "express-rate-limit";
 import logger from "../utils/logger.js";
 
-/* Restriction des requêtes pour prévenir le spam (5 msg / 15 min) */
+/* Restriction des requêtes pour prévenir le spam (2 msg / 1 h) */
 export const contactLimiter = rateLimit({
      windowMs: 60 * 60 * 1000,
     max: 2,
