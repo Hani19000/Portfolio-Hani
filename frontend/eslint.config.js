0fa7f1175086b97bc89da6daf7fc51383a3c1038
Hani <hanider27@gmail.com>
2026-01-10 01:12:14 +0100

    mise a jours

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,30 +2,33 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint' // Nécessaire en TS
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules'] },
   {
-    files: ['**/*.{js,jsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx,js,jsx}'], // Ajout du support TS
     languageOptions: {
       ecmaVersion: 2020,
-      // On combine les globales du navigateur ET de Node.js
       globals: {
         ...globals.browser,
-        ...globals.node, 
-      },
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        ...globals.node,
       },
     },
-    // Note: Le bloc "extends" doit être à plat dans la config ou via js.configs
+
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     rules: {
-      ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'no-unused-vars': 'off', 
+      '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
-])
+)
