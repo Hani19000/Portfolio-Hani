4f263f3cb5c31a8e21339fae53dce31b8ecdd141
Hani <hanider27@gmail.com>
2026-01-14 19:11:01 +0100

    add particales react bits

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useEffect } from "react";
+import { lazy, Suspense, useEffect, useState } from "react"; // Ajout de useState
 import Header from "./pages/Header";
 import Nav from "./pages/Nav";
 import { ReveilServer } from "./api/api";
@@ -6,7 +6,6 @@ import Loading from "./components/Loading";
 import Particles from "./hooks/Particles";
 import "./styles/particales.css";
 
-// Composants chargés à la demande
 const About = lazy(() => import("./pages/About"));
 const Experience = lazy(() => import("./pages/Experience"));
 const Services = lazy(() => import("./pages/Services"));
@@ -21,32 +20,41 @@ const SectionLoader = () => (
 );
 
 function App() {
+  // --- DETECTION MOBILE ---
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
   useEffect(() => {
     ReveilServer();
+
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
     <>
-      <div
-        style={{
-          position: "fixed",
-          inset: 0,
-          zIndex: -1,
-          pointerEvents: "none",
-          background: "transparent",
-        }}
-      >
-        <Particles
-          particleColors={["#ffffff", "#ffffff"]}
-          particleCount={250}
-          particleSpread={10}
-          speed={0.1}
-          particleBaseSize={100}
-          moveParticlesOnHover={true}
-          alphaParticles={true}
-          disableRotation={false}
-        />
-      </div>
+      {!isMobile && (
+        <div
+          style={{
+            position: "fixed",
+            inset: 0,
+            zIndex: -1,
+            pointerEvents: "none",
+            background: "transparent",
+          }}
+        >
+          <Particles
+            particleColors={["#ffffff", "#ffffff"]}
+            particleCount={250}
+            particleSpread={10}
+            speed={0.1}
+            particleBaseSize={100}
+            moveParticlesOnHover={true}
+            alphaParticles={true}
+            disableRotation={false}
+          />
+        </div>
+      )}
 
       <Nav />
       <main style={{ position: "relative", zIndex: 1 }}>
